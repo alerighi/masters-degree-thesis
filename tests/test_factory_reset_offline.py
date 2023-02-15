4db49c720437a0b54141a832fa436d7d57ba3fd2
alerighi <alessandro.righi@alerighi.it>
2023-02-15 18:01:43 +0100

    improve factory reset tests

--- tests/test_factory_reset_offline.py
+++ tests/test_factory_reset_offline.py
@@ -4,11 +4,11 @@ import uuid
 
 from fw_test.context import Context
 from fw_test.io import LedColor
-from fw_test.cloud import Action, Response, Message
+from fw_test.cloud import Action, Message, Response
 
 from .utils import TEST_AP_CONFIG, STATE_MANUAL
 
-def test_factory_reset(ctx: Context):
+def test_factory_reset_offline(ctx: Context):
     
     # avvio la connessione del Raspberry all'AP
     ctx.wifi.client_connect()
@@ -38,19 +38,18 @@ def test_factory_reset(ctx: Context):
         }
     ))
 
-    msg = ctx.cloud.receive(timeout=30)
-    assert msg.action == Action.REPORTED_UPDATE
+    sleep(1)
 
     assert ctx.io.status_led_color() == LedColor.OFF
 
+    # ora stoppo l'access-point
+    ctx.wifi.stop_ap()
+
+    # attendo 5 secondi
+    sleep(5)
+
     # ora posso fare l'hard reset 
     ctx.io.hard_reset()
-
-    # attendo che arrivi su cluod una REPORTED UPDATE con envId 
-    # vuoto (rimozione abbinamento)
-    msg = ctx.cloud.receive(timeout=5)
-    assert msg.action == Action.REPORTED_UPDATE
-    assert msg.state["envId"] == b"\0" * 16
 
     sleep(1)
 
